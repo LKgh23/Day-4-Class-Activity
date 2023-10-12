4775f77476483a3c27647aaa421b466a6437ff46
Liam McDonnell <liammcdonnell@liams-mbp-3.dhcp.nd.edu>
2023-10-12 10:22:08 -0400

    Made filtered dataset

--- Rail Equip Analysis.R
+++ Rail Equip Analysis.R
@@ -1,3 +1,7 @@
-read.csv("~//Downloads/Rail_Equipment_Accident_Incident_Data.csv")
-
 Trains <- read.csv('~//Downloads/Rail_Equipment_Accident_Incident_Data.csv')
+
+Trains$Total.Casualties <- Trains$Total.Persons.Injured + Trains$Total.Persons.Killed
+
+Trains_Filtered <- Trains[ , c("Accident.Number", "Report.Year", "Reporting.Railroad.Name", "Hazmat.Cars", "Persons.Evacuated", "State.Name", "Temperature", "Visibility", "Weather.Condition", "Train.Speed", "Gross.Tonnage", "Positive.Alcohol.Tests", "Positive.Drug.Tests", "Total.Damage.Cost", "Total.Casualties", "Longitude", "Latitude")] ##
+
+write.csv(Trains_Filtered, "~//Downloads/Filtered_Trains_Dataset.csv")
